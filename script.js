f0c627a7d250111306557f65e7b63398205240bf
Adena McGibbon <justadena@gmail.com>
2019-10-10 16:30:45 -0600

    Added JS file and pseudo code.

---
+++ script.js
@@ -0,0 +1,17 @@
+//In my HTML I'll need div that will hold the time table
+//I div or header that shows the current day
+  //js has a DateObject() built in
+
+//Input fields inside the div for the eight hours in the day
+  //When the page loads, check local storage,
+  //if anything exists, load it into the corresponding time slot
+
+
+//I'll need a method for capturing the user input and saving it in local storage
+  //each input field will need it's own save button
+
+//I'll need way to the current day and time.  
+
+//A formula that will color code an hour that has passed
+
+//
