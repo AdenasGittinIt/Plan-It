45ef6b82ddef2d4960ce1d28d1bbf6f51c737e2a
Adena McGibbon <justadena@gmail.com>
2019-10-11 20:13:35 -0600

    Added basic HTML. Created code to insert the day of the week on the page.

--- script.js
+++ script.js
@@ -2,6 +2,18 @@
 //I div or header that shows the current day
   //js has a DateObject() built in
 
+  var d = new Date();
+  var dayIndex = d.getDay();
+  var date = d.getDate();
+
+  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+  var currentDay = days[dayIndex];
+  $("#today").html(currentDay);
+
+  console.log(currentDay);
+  console.log(date);
+
 //Input fields inside the div for the eight hours in the day
   //When the page loads, check local storage,
   //if anything exists, load it into the corresponding time slot
