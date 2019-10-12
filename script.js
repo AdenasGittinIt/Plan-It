59d76e7bfcaae73aa1725188e22014c72d1f95e5
Adena McGibbon <justadena@gmail.com>
2019-10-11 21:59:00 -0600

    Added variables to store the current hour and minute with a console.log to confirm that they are working

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 //In my HTML I'll need div that will hold the time table
 //A div or header that shows the current day
   //js has a DateObject() built in
+
   var d = new Date();
+  var minutes = d.getMinutes();
+  var hour = d.getHours();
   var date = d.getDate();
   var dayIndex = d.getDay();
+
+  console.log(hour + ":" + minutes)
   
   var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
   var today = days[dayIndex];
@@ -15,21 +20,21 @@
   var month = allMonths[monthIndex];
   var year = d.getFullYear();
  
-  $("#today").html(today +" " + month +" " + date + ", " + year);
-
-  console.log(year);
-  console.log(month);
+  $("#today").text(today +" " + month +" " + date + ", " + year);
 
 //Input fields inside the div for the eight hours in the day
   //When the page loads, check local storage,
   //if anything exists, load it into the corresponding time slot
 
 
+
+
 //I'll need a method for capturing the user input and saving it in local storage
   //each input field will need it's own save button
 
-//I'll need way to the current day and time.  
+//I'll need way to track the current day and time.  
 
 //A formula that will color code an hour that has passed
 
-//
+//A conditional statment.
+// if the div id is greater than or equal to the time style background color green else color grey
