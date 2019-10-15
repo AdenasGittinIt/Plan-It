47fb45c98a8ef3a20e112d829a6a10a0b7deb1d0
Adena McGibbon <justadena@gmail.com>
2019-10-14 21:49:08 -0600

    The event listener sets the value of the input field in the DOM but does not persist after page reload.

---
+++ pseudoCode.js
@@ -0,0 +1,4 @@
+//In my HTML I'll need div that will hold the time table
+//A div or header that shows the current day
+  //js has a DateObject() built in
+  //Input fields inside the div for the eight hours in the day
--- script.js
+++ script.js
@@ -1,36 +1,45 @@
-//In my HTML I'll need div that will hold the time table
-//A div or header that shows the current day
-  //js has a DateObject() built in
-
   var d = new Date();
   var minutes = d.getMinutes();
   var hour = d.getHours();
   var date = d.getDate();
   var dayIndex = d.getDay();
-
-  console.log(hour + ":" + minutes)
+  var timeNow = hour + ":" + minutes;
+  console.log(timeNow);
   
   var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
   var today = days[dayIndex];
 
   var monthIndex = d.getMonth();
-  
-  var allMonths = ["January",
-    "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
   var month = allMonths[monthIndex];
   var year = d.getFullYear();
  
   $("#today").text(today +" " + month +" " + date + ", " + year);
 
-//Input fields inside the div for the eight hours in the day
-  //When the page loads, check local storage,
-  //if anything exists, load it into the corresponding time slot
 
+//When the page loads, check local storage,
+//if anything exists, load it into the corresponding time slot
+//This will be a .get function??? I'll need to loop through an array get and display an values?
 
 
 
 //I'll need a method for capturing the user input and saving it in local storage
-  //each input field will need it's own save button
+//each input field will need it's own save button
+
+  var inputArray = $(".hour-input");
+
+
+  $(".button").click(function(e) {
+  for (i = 0; i < inputArray.length; i++) {
+    localStorage.setItem("appt", inputArray[i].value);
+  }
+  
+  console.log(inputArray);
+
+  });
+   
+
 
 //I'll need way to track the current day and time.  
 
