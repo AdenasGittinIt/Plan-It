4476cef0d893b9e64bb8a94e1812e531113ef034
Adena McGibbon <justadena@gmail.com>
2019-10-15 17:24:54 -0600

    the click event function saves to local stroage but only saves the last item input

--- pseudoCode.js
+++ pseudoCode.js
@@ -1,4 +1,7 @@
 //In my HTML I'll need div that will hold the time table
 //A div or header that shows the current day
-  //js has a DateObject() built in
-  //Input fields inside the div for the eight hours in the day
+//js has a DateObject() built in
+//Input fields inside the div for the eight hours in the day
+
+//When the page loads, check local storage,
+//if appointments exists, load it into the corresponding time slot
--- script.js
+++ script.js
@@ -1,39 +1,39 @@
-  var d = new Date();
-  var minutes = d.getMinutes();
-  var hour = d.getHours();
-  var date = d.getDate();
-  var dayIndex = d.getDay();
-  var timeNow = hour + ":" + minutes;
-  console.log(timeNow);
+var d = new Date();
+var minutes = d.getMinutes();
+var hour = d.getHours();
+var date = d.getDate();
+var dayIndex = d.getDay();
+var timeNow = hour + ":" + minutes;
+
+console.log(timeNow);
   
-  var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-  var today = days[dayIndex];
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+var today = days[dayIndex];
 
-  var monthIndex = d.getMonth();
-  var allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var monthIndex = d.getMonth();
+var allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-  var month = allMonths[monthIndex];
-  var year = d.getFullYear();
- 
-  $("#today").text(today +" " + month +" " + date + ", " + year);
+var month = allMonths[monthIndex];
+var year = d.getFullYear();
+var inputArray = $(".hour-input");
 
+$("#today").text(today +" " + month +" " + date + ", " + year);
+  
 
-//When the page loads, check local storage,
-//if anything exists, load it into the corresponding time slot
-//This will be a .get function??? I'll need to loop through an array get and display an values?
+// $().ready(function() {
+//   for (i = 0; i < inputArray.length; i++) {
+//     var appointment = localStorage.getItem("appt");
+//     inputArray[i].value appointment
+//   }
+// });
 
-
-
-//I'll need a method for capturing the user input and saving it in local storage
+//I'll need a function to capture the user input and saving it in local storage
 //each input field will need it's own save button
-
-  var inputArray = $(".hour-input");
-
 
   $(".button").click(function(e) {
   for (i = 0; i < inputArray.length; i++) {
     localStorage.setItem("appt", inputArray[i].value);
-  }
+  };
   
   console.log(inputArray);
 
